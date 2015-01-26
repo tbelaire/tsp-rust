65644d63d13a99a8c283b5fe492726613883a430
Theo Belaire <theo.belaire@gmail.com>
2015-01-26 14:04:39 -0500

    Brute force working, 500 times slower

--- src/main.rs
+++ src/main.rs
@@ -52,6 +52,24 @@ pub mod graph {
             }
             len
         }
+
+        pub fn print_tour(&self, path: &Vec<usize>) {
+            let mut it = path.iter();
+            let first = it.next();
+            match first {
+                Some(i) => {
+                    let (c, _, _) = self.nodes[*i];
+                    print!("[{}", c);
+                    for i in it {
+                        let (c, _, _) = self.nodes[*i];
+                        print!(", {}", c);
+                    }
+                    print!("]")
+                },
+                None => print!("[]"),
+            }
+        }
+
     }
 
     #[test]
@@ -68,12 +86,39 @@ pub mod graph {
 
 pub mod search {
     use super::graph;
+    use std::usize;
+
     pub fn enumerate_all(g: graph::Graph) -> usize{
-        let mut perm:Vec<usize> = Vec::new();
-        let mut best_toup:Vec<usize> = Vec::new();
-        let mut best_distance = 0;
+        let mut perm:Vec<usize> = range(0,g.len()).collect();
+        let mut best_tour:Vec<usize> = Vec::new();
+        let mut best_distance = usize::MAX;
+        fn permute(perm: &mut Vec<usize>,
+                   best_tour: &mut Vec<usize>,
+                   best_distance: &mut usize,
+                   g: &graph::Graph, k: usize) {
+            if k == 1 {
+                let len = g.tour_length(perm);
+                // print!("Tour: ");
+                // g.print_tour(perm);
+                // println!(" of length {}", len);
 
-
+                if len < *best_distance {
+                    *best_distance = len;
+                    *best_tour = perm.clone();
+                }
+            } else {
+                for i in range(0, k) {
+                    perm.swap(i, k-1);
+                    permute(perm, best_tour, best_distance, g, k-1);
+                    perm.swap(i, k-1)
+                }
+            }
+        }
+        permute(&mut perm, &mut best_tour, &mut best_distance, &g, g.len());
+        print!("Best tour is:");
+        g.print_tour(&best_tour);
+        println!("");
+        best_distance
 
     }
 
