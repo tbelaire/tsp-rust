76ad34f36bdf91abbd632f07a3c9f5aad0d5d03a
Theo Belaire <theo.belaire@gmail.com>
2015-01-26 13:13:35 -0500

    Now starting search

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![feature(box_syntax)]
+#![allow(unstable)]
 extern crate argparse;
 
 use std::collections::BinaryHeap;
@@ -9,10 +10,72 @@ use argparse::{ArgumentParser, StoreTrue, Store};
 
 
 pub mod graph {
-    pub type Graph = Vec<(char, i32, i32)>;
+    use std::num::Float;
+    use std::num::Int;
+    pub struct Graph {
+        size: usize,
+        nodes: Vec<(char, i32, i32)>,
+        distances: Vec<Vec<usize>>
+    }
+    impl Graph {
+        pub fn new(v : Vec<(char, i32, i32)>) -> Graph {
+            let dist =  v.iter().map(
+                    |&(_, ax, ay)| {
+                        v.iter().map(|&(_, bx, by)| {
+                            let d2:f32 = ((ax - bx).pow(2) + (ay - by).pow(2)) as f32;
+                            (d2.sqrt() + 0.5) as usize
+                        }).collect()
+                    }).collect();
+            Graph{
+                size: v.len(),
+                nodes: v,
+                distances: dist
+            }
+        }
+        pub fn len(&self) -> usize {
+            self.size
+        }
+
+        pub fn tour_length(&self, path: &Vec<usize>) -> usize{
+            let mut len = 0;
+            for (a,b) in path.iter().zip(
+                    path.iter().cycle().skip(1)) {
+                len += self.distances[*a][*b];
+            }
+            len
+        }
+        pub fn path_length(&self, path: &Vec<usize>) -> usize{
+            let mut len = 0;
+            for (a,b) in path.iter().zip(
+                    path.iter().skip(1)) {
+                len += self.distances[*a][*b];
+            }
+            len
+        }
+    }
+
+    #[test]
+    fn path_test(){
+        let g = Graph::new(vec![('a', 0,0), ('b', 0,4), ('c', 4,4), ('d', 4,0)]);
+        let perm = vec!(0,1,2,3);
+        assert_eq!(g.tour_length(&perm), 16);
+        let perm = vec!(0,1);
+        assert_eq!(g.tour_length(&perm), 8);
+        assert_eq!(g.path_length(&perm), 4);
+
+    }
 }
 
 pub mod search {
+    use super::graph;
+    pub fn enumerate_all(g: graph::Graph) -> usize{
+        let mut perm:Vec<usize> = Vec::new();
+        let mut best_toup:Vec<usize> = Vec::new();
+        let mut best_distance = 0;
+
+
+
+    }
 
 }
 
@@ -46,17 +109,16 @@ fn main() {
 
     let mut ls = file.lines();
     let n_str = ls.next();
-    let graph_lst:graph::Graph = ls.map(|line| {
+    let graph_lst:graph::Graph = graph::Graph::new(ls.map(|line| {
         let l2 = line.unwrap();
+        // Trim removes the trailing whitespace
         let words:Vec<&str> = l2.trim().split_str(" ").collect();
-        let c:char = words[0].chars().next().unwrap();
-        let x:i32 = words[1].parse().unwrap();
-        let y:i32 = words[2].parse().unwrap();
+        let c:char = words[0].chars().next()
+                        .expect("Failed to parse city name");
+        let x:i32 = words[1].parse().expect("Failed to parse x co-ordinate");
+        let y:i32 = words[2].parse().expect("Failed to parse y co-ordinate");
         (c,x,y)
-    }).collect();
-    if verbose {
-        for &(c, x, y) in graph_lst.iter() {
-            println!("{} {} {}", c, x, y);
-        }
-    }
+    }).collect());
+
+    println!("Best path is {}", search::enumerate_all(graph_lst));
 }
