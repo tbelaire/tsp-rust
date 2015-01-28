c329eae0725ecd7363d98753ed97be881f9b5b24
Theo Belaire <theo.belaire@gmail.com>
2015-01-28 13:21:44 -0500

    Now up to rand_tour4
    I avoid re-computing the distances

--- src/graph.rs
+++ src/graph.rs
@@ -3,28 +3,36 @@ use std::num::Int;
 pub struct Graph {
     size: usize,
     nodes: Vec<(char, i32, i32)>,
-    distances: Vec<Vec<usize>>
+    distances: Vec<Vec<usize>>,
+    min_cost_edges: Vec<usize>
+
 }
 impl Graph {
     pub fn new(v : Vec<(char, i32, i32)>) -> Graph {
-        let dist =  v.iter().map(
+        let dist:Vec<Vec<usize>> = v.iter().map(
                 |&(_, ax, ay)| {
                     v.iter().map(|&(_, bx, by)| {
                         let d2:f32 = ((ax - bx).pow(2) + (ay - by).pow(2)) as f32;
                         (d2.sqrt() + 0.5) as usize
                     }).collect()
                 }).collect();
+        let mce:Vec<usize> = dist.iter().map(
+            // Note, if there are cities on top of each other, the min cost is
+            // wrong
+            |ds|->usize *(ds.iter().filter(|&&d| d!= 0).min().unwrap())).collect();
         Graph{
             size: v.len(),
             nodes: v,
-            distances: dist
+            distances: dist,
+            min_cost_edges: mce
+
         }
     }
     pub fn len(&self) -> usize {
         self.size
     }
 
-    pub fn tour_length(&self, path: &Vec<usize>) -> usize{
+    pub fn tour_length(&self, path: &[usize]) -> usize {
         let mut len = 0;
         for (a,b) in path.iter().zip(
                 path.iter().cycle().skip(1)) {
@@ -32,7 +40,7 @@ impl Graph {
         }
         len
     }
-    pub fn path_length(&self, path: &Vec<usize>) -> usize{
+    pub fn path_length(&self, path: &[usize]) -> usize {
         let mut len = 0;
         for (a,b) in path.iter().zip(
                 path.iter().skip(1)) {
@@ -41,7 +49,7 @@ impl Graph {
         len
     }
 
-    pub fn print_tour(&self, path: &Vec<usize>) {
+    pub fn print_tour(&self, path: &[usize]) {
         let mut it = path.iter();
         let first = it.next();
         match first {
@@ -58,15 +66,36 @@ impl Graph {
         }
     }
 
+    pub fn lower_bound_tour_mce(&self, path: &[usize], k:usize)
+        -> usize {
+            let p = self.path_length(&path[0..k]);
+
+            let s = path[k..].iter()
+                .fold(0, |sum, &i| sum + self.min_cost_edges[i]);
+                // .fold(0, |sum, &i| sum + i);
+            p+s
+    }
+    pub fn dist(&self, a : usize, b : usize) -> usize {
+        self.distances[a][b]
+    }
+
 }
 
 #[test]
 fn path_test(){
     let g = Graph::new(vec![('a', 0,0), ('b', 0,4), ('c', 4,4), ('d', 4,0)]);
     let perm = vec!(0,1,2,3);
-    assert_eq!(g.tour_length(&perm), 16);
+    assert_eq!(g.tour_length(perm.as_slice()), 16);
     let perm = vec!(0,1);
-    assert_eq!(g.tour_length(&perm), 8);
-    assert_eq!(g.path_length(&perm), 4);
+    assert_eq!(g.tour_length(perm.as_slice()), 8);
+    assert_eq!(g.path_length(perm.as_slice()), 4);
 
 }
+
+#[test]
+fn mce_test(){
+    let g = Graph::new(vec![('a', 0,0), ('b', 0,4), ('c', 4,4), ('d', 4,0)]);
+    let perm = vec!(0,1,2,3);
+    assert_eq!(g.lower_bound_tour_mce(perm.as_slice(), 2), 16);
+}
+
--- src/search.rs
+++ src/search.rs
@@ -4,32 +4,41 @@ use std::usize;
 pub fn enumerate_all(g: graph::Graph) -> usize{
     let mut perm:Vec<usize> = range(0,g.len()).collect();
     let mut best_tour:Vec<usize> = Vec::new();
+    // let mut best_tour = [0; g.len()];
     let mut best_distance = usize::MAX;
     fn permute(perm: &mut Vec<usize>,
                 best_tour: &mut Vec<usize>,
                 best_distance: &mut usize,
-                g: &graph::Graph, k: usize) {
+                g: &graph::Graph, k: usize,
+                tourlen: usize) {
         if k == 1 {
-            let len = g.tour_length(perm);
+            // let len = g.tour_length(perm.as_slice());
+            let len = (tourlen + g.dist(perm[0], perm[1])
+                       + g.dist(perm[g.len()-1], perm[0]));
+
+
             // print!("Tour: ");
             // g.print_tour(perm);
             // println!(" of length {}", len);
 
             if len < *best_distance {
                 *best_distance = len;
-                *best_tour = perm.clone();
+                best_tour.clone_from(perm);
             }
         } else {
             for i in range(0, k) {
                 perm.swap(i, k-1);
-                permute(perm, best_tour, best_distance, g, k-1);
+                let a = perm[k-1];
+                let b = perm[k];
+                permute(perm, best_tour, best_distance, g, k-1,
+                       tourlen + g.dist(a, b));
                 perm.swap(i, k-1)
             }
         }
     }
-    permute(&mut perm, &mut best_tour, &mut best_distance, &g, g.len());
+    permute(&mut perm, &mut best_tour, &mut best_distance, &g, g.len()-1, 0);
     print!("Best tour is: ");
-    g.print_tour(&best_tour);
+    g.print_tour(best_tour.as_slice());
     println!("");
     best_distance
 
